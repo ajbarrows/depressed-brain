35924bb202af69b0942184d05b2db98f605771e8
Anthony Barrows <ajbarrow@uvm.edu>
2026-02-02 14:20:50 -0500

    add new plotting and reporting (quarto)

--- scripts/generate_plots.R
+++ scripts/generate_plots.R
@@ -9,10 +9,23 @@ reshaped <- reshape_for_plotting(df)
 brain_reshaped <- reshaped %>% reshape_for_brain_plotting()
 
 plot_cbcl_by_sex(reshaped)
-ggsave(paste0(plot_path, "cbcl_by_sex.png"), dpi = 'retina', height=4)
+ggsave(paste0(plot_path, "mh_by_sex.png"), dpi = 'retina', height=4)
 
 plot_mh_by_brain(brain_reshaped, structure_string = "Hippocampus")
-ggsave(paste0(plot_path, "mh_by_hippocampus.png"), dpi = "retina", width=10)
+ggsave(paste0(plot_path, "mh_by_hippocampus.png"), dpi = "retina", width=10, height=12)
 
 plot_mh_by_brain(brain_reshaped, structure_string = "Amygdala")
-ggsave(paste0(plot_path, "mh_by_amygdala.png"), dpi = "retina", width=10)
+ggsave(paste0(plot_path, "mh_by_amygdala.png"), dpi = "retina", width=10, height=12)
+
+make_pairplot(df, outcomes_map, "Depression Symptoms (all time points)")
+ggsave("./reports/figures/mh_correlation_all.png", dpi = 'retina', width=8, height=8)
+
+tpts <- c("baseline", "year_2", "year_4", "year_6")
+fpath <- "./reports/figures/"
+for (tpt in tpts) {
+  title <- paste0("Depression Symptoms (", tpt, ")")
+  fname <- paste0(fpath, "mh_correlation_", tpt, ".png")
+  
+  make_pairplot(df, outcomes_map, title)
+  ggsave(fname, dpi = "retina", width=8, height=8)
+}
