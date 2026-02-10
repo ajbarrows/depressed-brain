8be5b11febe037d3c212193aaaa993afd71148bc
Anthony Barrows <ajbarrow@uvm.edu>
2026-02-10 14:39:15 -0500

    modify data management to have a limited subset

--- R/data.R
+++ R/data.R
@@ -81,17 +81,6 @@ filter_mri_qc <- function(df) {
     filter(mr_y_qc__incl__smri__t1_indicator == 1)
 }
 
-join_6mo_bpm <- function(df) {
-  tmp <- df %>%
-    filter(session_id == 'ses-00M') %>%
-    select(participant_id, contains("bpm__int"))
-  
-  df %>%
-    left_join(tmp, by = "participant_id", suffix = c("", "_6mo")) %>%
-    mutate(across(contains("bpm__int") & !contains("_6mo"), 
-                  ~coalesce(., get(paste0(cur_column(), "_6mo")))))
-}
-
 # figure out what day the ASR was collected
 extract_corresponding_day <- function(df) {
   df %>%
@@ -117,7 +106,6 @@ extract_corresponding_day <- function(df) {
     ) %>%
     filter(mh_p_asr_dtt == date | num_visit_days  == 1) %>%
     mutate(asr_day = stringr::str_extract(date_var, "day\\d+"))
-  # select(participant_id, session_id, asr_day, date, mh_p_asr_dtt)
 }
 limit_to_bio_mom <- function(df, timepoint = 'ses-00A') {
 
@@ -134,7 +122,6 @@ limit_to_bio_mom <- function(df, timepoint = 'ses-00A') {
       by = c("participant_id", "session_id"),
       relationship = "many-to-many"
     )  %>%
-    #  tidyr::drop_na() %>%
     filter(inform_day == asr_day | num_visit_days  == 1) %>%
     distinct(participant_id, session_id, .keep_all = TRUE) %>%
     select(
@@ -152,7 +139,6 @@ limit_to_bio_mom <- function(df, timepoint = 'ses-00A') {
     filter(session_id == timepoint) %>%
     select(
       participant_id,
-      # session_id,
       baseline_maternal_asr_int = mh_p_asr__synd__int_sum,
       baseline_maternal_asr_anxdep = mh_p_asr__synd__anxdep_sum
     )
@@ -190,58 +176,131 @@ summarize_ksads_dep_sx <- function(df) {
 }
 
 
+# format_subset <- function(df, timepoint_map, sex_map) {
+#   df %>%
+#     select(
+#       participant_id,
+#       session_id,
+#       # sex = ab_g_stc__cohort_sex,
+#       # age = ab_g_dyn__visit_age,
+#       # site = ab_g_dyn__design_site,
+#       # scanner_serial = ab_g_dyn__design_mr__serial,
+#       # family_id = ab_g_stc__design_id__fam,
+#       # anxdep = mh_p_cbcl__synd__anxdep_sum,
+#       # internal = mh_p_cbcl__synd__int_sum,
+#       # attention = mh_p_cbcl__synd__attn_sum,
+#       # left_amygdala_vol = mr_y_smri__vol__aseg__ag__lh_sum,
+#       # right_amygdala_vol = mr_y_smri__vol__aseg__ag__rh_sum,
+#       # left_hippocampus_vol = mr_y_smri__vol__aseg__hc__lh_sum,
+#       # right_hippocampus_vol = mr_y_smri__vol__aseg__hc__rh_sum,
+#       ab_g_stc__cohort_sex,
+#       ab_g_dyn__visit_age,
+#       ab_g_dyn__design_site,
+#       ab_g_stc__design_id__fam,
+#       mh_p_cbcl__synd__anxdep_sum,
+#       mh_p_cbcl__synd__int_sum,
+#       mh_p_cbcl__synd__attn_sum,
+#       mh_p_cbcl__synd__wthdep_sum,
+#       mh_p_cbcl__dsm__dep_sum,
+#       mh_t_bpm__int_sum,
+#       mh_y_bpm__int_sum,
+#       mr_y_smri__vol__aseg__ag__lh_sum,
+#       mr_y_smri__vol__aseg__ag__rh_sum,
+#       mr_y_smri__vol__aseg__hc__lh_sum,
+#       mr_y_smri__vol__aseg__hc__rh_sum,
+#       mr_y_smri__vol__aseg__icv_sum,
+#       mr_y_adm__info__dev_serial,
+#       baseline_maternal_asr_int,
+#       baseline_maternal_asr_anxdep,
+#       mh_y_ksads__dep__mdd__pres_dx,
+#       mh_y_ksads__dep__pdd__pres_dx,
+#       total_ksads_dep_sx
+#     ) %>%
+#     mutate(
+#       session_id = recode(session_id, !!!timepoint_map),
+#       ab_g_stc__cohort_sex = recode(ab_g_stc__cohort_sex, !!!sex_map),
+#       mr_y_adm__info__dev_serial = factor(mr_y_adm__info__dev_serial)
+#     ) %>%
+#     arrange(participant_id, session_id) %>%
+#     tidyr::drop_na(!(starts_with("mh_y_bpm")))
+# }
 
 
-
-format_subset <- function(df, timepoint_map, sex_map) {
+subset_baseline <- function(df) {
   df %>%
+    filter(session_id == 'baseline') %>%
+    select(
+      participant_id,
+      sex,
+      age = ab_g_dyn__visit_age,
+      site = ab_g_dyn__design_site,
+      scanner_serial,
+      family_id = ab_g_stc__design_id__fam,
+      baseline_maternal_asr_int,
+      baseline_maternal_asr_anxdep
+    )
+}
+
+subset_timepoint <- function(
+  df,
+  timepoints = c("year_2"),
+  var_string = "mr_y_smri"
+) {
+  n_vars <- df %>% select(contains(var_string)) %>% ncol()
+
+  tmp <- df %>%
+    filter(session_id %in% timepoints) %>%
+    filter_mri_qc() %>%
     select(
       participant_id,
       session_id,
-      # sex = ab_g_stc__cohort_sex,
-      # age = ab_g_dyn__visit_age,
-      # site = ab_g_dyn__design_site,
-      # scanner_serial = ab_g_dyn__design_mr__serial,
-      # family_id = ab_g_stc__design_id__fam,
-      # anxdep = mh_p_cbcl__synd__anxdep_sum,
-      # internal = mh_p_cbcl__synd__int_sum,
-      # attention = mh_p_cbcl__synd__attn_sum,
-      # left_amygdala_vol = mr_y_smri__vol__aseg__ag__lh_sum,
-      # right_amygdala_vol = mr_y_smri__vol__aseg__ag__rh_sum,
-      # left_hippocampus_vol = mr_y_smri__vol__aseg__hc__lh_sum,
-      # right_hippocampus_vol = mr_y_smri__vol__aseg__hc__rh_sum,
-      ab_g_stc__cohort_sex,
-      ab_g_dyn__visit_age,
-      ab_g_dyn__design_site,
-      ab_g_stc__design_id__fam,
-      mh_p_cbcl__synd__anxdep_sum,
-      mh_p_cbcl__synd__int_sum,
-      mh_p_cbcl__synd__attn_sum,
-      mh_p_cbcl__synd__wthdep_sum,
-      mh_p_cbcl__dsm__dep_sum,
-      mh_t_bpm__int_sum,
-      mh_y_bpm__int_sum,
-      mr_y_smri__vol__aseg__ag__lh_sum,
-      mr_y_smri__vol__aseg__ag__rh_sum,
-      mr_y_smri__vol__aseg__hc__lh_sum,
-      mr_y_smri__vol__aseg__hc__rh_sum,
-      mr_y_smri__vol__aseg__icv_sum,
-      mr_y_adm__info__dev_serial,
-      baseline_maternal_asr_int,
-      baseline_maternal_asr_anxdep,
-      mh_y_ksads__dep__mdd__pres_dx,
-      mh_y_ksads__dep__pdd__pres_dx,
-      total_ksads_dep_sx
+      contains(var_string)
     ) %>%
+    tidyr::pivot_wider(
+      id_cols = participant_id,
+      names_from = session_id,
+      values_from = contains(var_string)
+    )
+
+  if (n_vars < 2) {
+    return(
+      tmp %>%
+        rename_with(
+          ~ paste0(var_string, "_", .x),
+          .cols = -c(participant_id)
+        )
+    )
+  } else {
+    return(tmp)
+  }
+}
+
+format_subset <- function(df, timepoint_map, sex_map) {
+  tmp <- df %>%
     mutate(
       session_id = recode(session_id, !!!timepoint_map),
-      ab_g_stc__cohort_sex = recode(ab_g_stc__cohort_sex, !!!sex_map),
-      mr_y_adm__info__dev_serial = factor(mr_y_adm__info__dev_serial)
+      sex = recode(ab_g_stc__cohort_sex, !!!sex_map),
+      scanner_serial = factor(mr_y_adm__info__dev_serial)
+    )
+  tmp %>%
+    subset_baseline() %>%
+    join_tables(
+      subset_timepoint(
+        tmp,
+        timepoints = c("year_2"),
+        var_string = "mr_y_smri"
+      )
     ) %>%
-    arrange(participant_id, session_id) %>%
-    tidyr::drop_na(!(starts_with("mh_y_bpm")))
-
+    join_tables(
+      subset_timepoint(
+        tmp,
+        timepoints = c("year_4"),
+        var_string = "mh_y_bpm__int"
+      )
+    ) %>%
+      tidyr::drop_na()
 }
+
 
 join_tables <- function(df, other, key = 'participant_id') {
   df %>%
@@ -264,7 +323,6 @@ load_data <- function(
   subset <- df %>%
     filter_mri_qc() %>%
     limit_to_bio_mom() %>%
-    join_6mo_bpm() %>%
     summarize_ksads_dep_sx() %>%
     format_subset(timepoint_map, sex_map)
 
